84524db6a950e7cd5364b8428989fca423188dc6
fatematujjohra <fatematujjohralima2@gmail.com>
2026-04-05 21:58:24 +0600

    set obj(obj convert to JSON.stringify then set)

--- js/local-storage.js
+++ js/local-storage.js
@@ -8,3 +8,10 @@ const getNumberFromLS = () => {
     const number = localStorage.getItem('number');
     console.log('from saved local storage', number)
 }
+
+const setObjectToLS = () => {
+    const customer = { name: 'rohim khalu', products: 3, price: 75 }
+    const customerJSON = JSON.stringify(customer)
+    localStorage.setItem('customer', customerJSON);
+
+}
