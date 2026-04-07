aa86d516a06c8e09e2182513a021ddd74010d60c
fatematujjohra <fatematujjohralima2@gmail.com>
2026-04-07 23:18:49 +0600

    display

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,22 @@ const handleAddProducts = () => {
     const productEl = document.getElementById('product');
     const quantityEl = document.getElementById('quantity');
     const product = productEl.value;
-    const quantity = (quantityEl.value);
-    console.log('product add', product, quantity);
-    
+    const quantity = parseInt(quantityEl.value);
+    // console.log('product add', product, quantity);
+
+    displayProduct(product, quantity);
+    addProductToCart(product, quantity)
+
+    productEl.value = '';
+    quantityEl.value = '';
+}
+
+
+const displayProduct = (product, quantity) =>{
+    const li = document.createElement('li');
+    li.innerText = `${product} : ${quantity}`
+
+    // get the ul
+    const ul = document.getElementById('products-container');
+    ul.appendChild(li);
 }
